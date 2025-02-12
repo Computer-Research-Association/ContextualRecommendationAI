204f0e843cc330df7596f4ef43577f2941017387
Jangmyun <cndlsrb2739@gmail.com>
2025-02-12 10:23:08 +0900

    [feat #5] CompletionItemProvider 구현

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,33 @@
 import * as vscode from "vscode";
-import { setAPIKey as setGemini } from "./utils";
+import { getAPIKey, setAPIKey as setGemini } from "./utils";
+import { getCodeContext } from "./generateRecommendations";
+import { GenerateContentResult, GenerativeModel, GoogleGenerativeAI } from "@google/generative-ai";
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  if (!context.globalState.get<boolean>("isValidGeminiAPIKey")) {
+    vscode.window.showErrorMessage("Please set your Gemini API Key", "Set API Key").then((value) => {
+      if (value === "Set API Key") {
+        setGemini(context);
+      }
+    });
+    context.globalState.update("isValidGeminiAPIKey", false);
+  }
+
   let statusBarVisible = true;
   let timeout: NodeJS.Timeout | undefined;
   const statusTextArray = ["Ready🦹‍♀️", "Typing💬"];
   const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
   statusBarItem.text = statusTextArray[0];
   statusBarItem.show();
+
+  let model: GenerativeModel | undefined;
+
+  if (context.globalState.get<boolean>("isValidGeminiAPIKey")) {
+    const APIKey = await getAPIKey(context);
+    model = new GoogleGenerativeAI(APIKey || "").getGenerativeModel({
+      model: "gemini-2.0-flash",
+    });
+  }
 
   const onTextChanged = vscode.workspace.onDidChangeTextDocument(() => {
     statusBarItem.text = statusTextArray[1];
@@ -21,12 +41,28 @@ export function activate(context: vscode.ExtensionContext) {
     }, 500);
   });
 
-  context.subscriptions.push(onTextChanged, statusBarItem);
   const setAPIKey = vscode.commands.registerCommand("extension.setAPIKey", async () => {
     await setGemini(context);
   });
 
-  context.subscriptions.push(setAPIKey);
+  vscode.languages.registerCompletionItemProvider(
+    { language: vscode.window.activeTextEditor?.document.languageId },
+    {
+      async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+        const codeContext = getCodeContext(document, position);
+        let res: GenerateContentResult | undefined;
+        if ((res = await model?.generateContent(codeContext))) {
+          const response = res.response.text();
+          console.log(response);
+          return [new vscode.CompletionItem(res.response.text())];
+        }
+
+        return;
+      },
+    }
+  );
+
+  context.subscriptions.push(onTextChanged, statusBarItem, setAPIKey);
 }
 
 export function deactivate() {}
